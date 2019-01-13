1e2dd15bdcc2dcfb4718afe851e54f186acd2f19
NickyWong <wxy-888@qq.com>
2019-01-13 11:58:28 +0800

    登录表单

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,55 @@
 <template>
-  <div id="app">
+  <div class="login-container">
+    <div class="form-container">
+      <h2>用户登录</h2>
+      <el-form label-position="top" label-width="80px" :model="formData">
+        <el-form-item label="用户名">
+          <el-input v-model="formData.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="formData.password"></el-input>
+        </el-form-item>
+        <el-button class="login-btn" type="primary">登录</el-button>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-}
+  name: "app",
+  data() {
+    return {
+      formData:{
+        name:'',
+        password:''
+      }
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.login-container {
+  height: 100%;
+  background-color: #324151;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form-container {
+  // height: 400px;
+  width: 580px;
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 20px;
+}
+.login-btn{
+  width: 100%;
+}
+
+h2 {
+  margin-top: 0;
 }
 </style>
